c1d1a0fe404c4b80af3c9d4056564e9e9b9f5cc5
Jordan Bottrell <Jordan.Bottrell@meijer.com>
2022-04-26 13:23:52 -0400

    added partyinvite project

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers{
+
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            //We don't need to specify a view because by default ASP.NET MVC looks for a view
+            //that matches the naming of the action method
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RsvpForm() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse) {
+            //TODO, actually store response from guests
+            return View();
+        }
+    }
+}
